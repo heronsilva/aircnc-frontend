22a653516bd7c5f4fafca3365ad06269b943144a
Heron Silva <heron.code@outlook.com>
2019-10-05 02:07:58 -0300

    Authentication form layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import './App.css';
 
+import logo from './assets/logo.svg';
+
 function App() {
   return (
-    <h1>AirCnC</h1>
+    <div className="container">
+      <img src={logo} alt="AirCnC" />
+
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e econtre <strong>talentos</strong> para sua empresa
+        </p>
+
+        <form>
+          <label htmlFor="email">E-MAIL *</label>
+
+          <input
+            type="email"
+            id="email"
+            placeholder="Seu melhor e-mail"
+            required
+          />
+
+          <button type="submit" className="btn">
+            Entrar
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
 
